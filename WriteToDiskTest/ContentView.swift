cd636fef03b47002c21f162febbe2ee185a07395
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2020-08-06 13:06:50 +0900

    Initial Commit

---
+++ WriteToDiskTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WriteToDiskTest
+//
+//  Created by h1d3mun3 on 8/6/20.
+//  Copyright © 2020 h1d3mun3. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WriteToDiskTestTests/WriteToDiskTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  WriteToDiskTestTests.swift
+//  WriteToDiskTestTests
+//
+//  Created by h1d3mun3 on 8/6/20.
+//  Copyright © 2020 h1d3mun3. All rights reserved.
+//
+
+import XCTest
+@testable import WriteToDiskTest
+
+class WriteToDiskTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
